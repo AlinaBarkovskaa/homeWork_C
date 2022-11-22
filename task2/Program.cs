5724cf9276d3d6e6303f747a1becefa58092363b
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-11-22 14:37:01 +0400

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите 2-ое число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine()); 
+if(number_1 > number_2)
+{
+   
+   Console.WriteLine("max = " + number_1 + " и " + "min = " + number_2);
+}
+else if(number_1 < number_2)
+{
+   
+   Console.WriteLine("max = " + number_2 + " и " + "min = " + number_1);
+}
+else
+{
+   Console.WriteLine("Оба числа равны");
+}
